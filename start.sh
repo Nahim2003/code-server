41f44a7f823c2880f32c9e8cb874a1c897bf5d84
Nahim2003 <nahimahmed1000@outlook.com>
2025-12-27 08:06:26 +0000

    Add http-proxy-middleware

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
+
 echo "Starting code-server..."
 code-server --bind-addr 0.0.0.0:8080 &
 
 echo "Starting node wrapper..."
-cd /app/wrapper/code-server
-node server.js
+node /app/wrapper/code-server/server.js
