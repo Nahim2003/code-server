c705323a7818fe455b8246ffc29b1faf7f65c6da
Nahim2003 <nahimahmed1000@outlook.com>
2026-06-13 16:54:43 +0100

    added new modules

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -22,3 +22,24 @@ resource "aws_iam_role_policy_attachment" "ecs_codeserver_task_execution_role" {
   role       = aws_iam_role.ecs_codeserver_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_iam_role_policy" "ecs_exec_ssm_messages" {
+  name = "ecs-exec-ssm-messages"
+  role = aws_iam_role.ecs_codeserver_task_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ssmmessages:CreateControlChannel",
+          "ssmmessages:CreateDataChannel",
+          "ssmmessages:OpenControlChannel",
+          "ssmmessages:OpenDataChannel"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
